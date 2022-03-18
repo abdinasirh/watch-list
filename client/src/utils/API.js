4342b4db208d6ecb6ec2dc76b38f7be783546d4e
Abdinasir Hassan <abdinasirduud20@gmail.com>
2022-03-18 09:40:41 -0500

    removed hardcoded api key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
 // make a search to movie api
-// example: Example: https://api.themoviedb.org/3/movie/550?api_key=fa5a36087590a648d382b06f3ec325df
+// example: Example: https://api.themoviedb.org/3/movie/550?api_key=
 export const searchMovies = (query) => {
     return fetch(`https://api.themoviedb.org/3/search/movie?q=${query}`);
   };
