a51910a1232f8d7da9c575b5ccdefd9ba472ceb7
Christopher Speltz <chrisspeltz@msn.com>
2022-03-21 21:58:56 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -28,6 +28,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/watch-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
